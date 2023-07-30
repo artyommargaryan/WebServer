50ce0e05838548ee1b6fc0c7c97e677449dbb3c3
artyommargaryan <artyom.margaryan.02@gmail.com>
2023-07-30 11:30:03 +0400

    updating imports

--- src/main/java/org/pregraduationhell/withoutAnnotation/Main.java
+++ src/main/java/org/pregraduationhell/withoutAnnotation/Main.java
@@ -1,6 +1,6 @@
 package org.pregraduationhell.withoutAnnotation;
 
-import org.pregraduationhell.withAnnotation.webServer.WebServer;
+import org.pregraduationhell.withoutAnnotation.webServer.WebServer;
 
 public class Main {
     public static void main(String[] args) {
